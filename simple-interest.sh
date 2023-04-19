0cb4b0efd300bd79b1fd1d409311e58913cb49df
Yesh1003 <114727466+Yesh1003@users.noreply.github.com>
2023-04-19 20:39:15 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Yesh1003>
    # Input:
    # p, principal amount
    # t, time period in years
